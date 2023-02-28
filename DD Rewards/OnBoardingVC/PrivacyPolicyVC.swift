af3744a40cf1d3c67300296306d90b7eb55e6ece
Ali Haidar <alihaidar679@gmail.com>
2023-02-28 13:22:45 +0500

    Privacy policy link

--- DD Rewards/OnBoardingVC/PrivacyPolicyVC.swift
+++ DD Rewards/OnBoardingVC/PrivacyPolicyVC.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 import DLRadioButton
-class PrivacyPolicyVC: UIViewController {
+class PrivacyPolicyVC: UIViewController, UITextViewDelegate {
     
     var privacySelected : Bool = false
 
     @IBOutlet weak var radioBtn: DLRadioButton!
     @IBOutlet weak var privacyPolicyAcceptBtn: UIButton!
-    @IBOutlet weak var privacyPolicyCheckLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var privacyPolicyCheckImage: UIImageView!
     @IBOutlet weak var privacyPolicyCenterImage: UIImageView!
     override func viewDidLoad() {
@@ -25,6 +25,17 @@ class PrivacyPolicyVC: UIViewController {
         privacyPolicyAcceptBtn.backgroundColor = UIColor.themeColor
         privacyPolicyAcceptBtn.tintColor = .white
         privacyPolicyAcceptBtn.layer.cornerRadius = 10
+    
+        let attributedString = NSMutableAttributedString(string: "I have read the privacy terms carefully and agree to the Privacy Policy")
+                attributedString.addAttribute(.link, value: "https://appsomagic.blogspot.com/p/privacy-policy-for-coin-master.html", range: NSRange(location: 57, length: 14))
+        textView.attributedText = attributedString
+        textView.font = .systemFont(ofSize: 16)
+
+
+    }
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+            UIApplication.shared.open(URL)
+            return false
     }
     
     @IBAction func radioButtonPressed(_ sender: UIButton) {
--- Pods/Target Support Files/Pods-DD Rewards/Pods-DD Rewards-frameworks.sh
+++ Pods/Target Support Files/Pods-DD Rewards/Pods-DD Rewards-frameworks.sh
@@ -179,6 +179,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DLRadioButton/DLRadioButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PanModal/PanModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
@@ -186,6 +187,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DLRadioButton/DLRadioButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PanModal/PanModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
